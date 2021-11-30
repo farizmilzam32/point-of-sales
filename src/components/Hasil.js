8604c196c1dc38188e87aa143be0768daa8ac258
farizmilzam32 <fariz.milzamh17@gmail.com>
2021-11-30 22:22:42 +0700

    commit

--- src/components/Hasil.js
+++ src/components/Hasil.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
-import { Col, Card, ListGroup } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 import TotalBayar from "./TotalBayar";
 export default class Hasil extends Component {
   render() {
-    const { keranjangs } = this.props;
     return (
       <Col md={3} className="mt-3">
         <h4>
